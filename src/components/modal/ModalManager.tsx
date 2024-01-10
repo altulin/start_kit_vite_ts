00c469a726ccf0d97535951a39b5150047022e67
Alexander <al.tulin@gmail.com>
2024-01-10 09:32:24 +0800

    modalManager optimized

--- src/components/modal/ModalManager.tsx
+++ src/components/modal/ModalManager.tsx
@@ -2,12 +2,15 @@ import { useAppSelector } from "@/hooks/hook";
 import ModalAuth1 from "./auth/ModalAuth1";
 import ModalError from "./error/ModalError";
 import useGetCurrentModal from "@/hooks/getCurrentModal";
+import { FC } from "react";
+import { IModalState } from "@/store/initialState";
 
-const ModalManager = () => {
-  const { modalState } = useAppSelector((state) => state.app);
+interface IModalElements {
+  modalState: IModalState;
+}
+
+const ModalElements: FC<IModalElements> = ({ modalState }) => {
   const modal = useGetCurrentModal(modalState);
-
-  if (!modalState) return null;
 
   return (
     <>
@@ -15,6 +18,11 @@ const ModalManager = () => {
       {modal === "error" && <ModalError open={true} />}
     </>
   );
+};
+
+const ModalManager = () => {
+  const { modalState } = useAppSelector((state) => state.app);
+  return modalState ? <ModalElements modalState={modalState} /> : null;
 };
 
 export default ModalManager;
