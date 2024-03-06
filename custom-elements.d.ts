fc8da6f8e3b7d99c279b11072bdd70f4f1880020
Alexander <al.tulin@gmail.com>
2024-03-06 11:25:46 +0800

    fix bugs

--- custom-elements.d.ts
+++
@@ -1,9 +0,0 @@
-// In a file like custom-elements.d.ts
-declare namespace JSX {
-  interface IntrinsicElements {
-    "swiper-container": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLDivElement>,
-      HTMLDivElement
-    >;
-  }
-}
