b2b1f9d2f9d44f16766e273e1de035331aab4dc1
Alexander <al.tulin@gmail.com>
2024-08-30 11:18:41 +0800

    fix

--- src/UI/form/hoc/Label.tsx
+++ src/UI/form/hoc/Label.tsx
@@ -2,12 +2,10 @@ import clsx from "clsx";
 import { FC } from "react";
 import style from "../Form.module.scss";
 import { ITextInput } from "../types";
-import { Field, useField } from "formik";
+import { Field } from "formik";
 
 const Label: FC<ITextInput> = ({ ...props }) => {
   const { modifier, label_text, id, children, ...input_props } = props;
-
-  const [meta] = useField(props.name || "");
 
   return (
     <label
@@ -29,7 +27,6 @@ const Label: FC<ITextInput> = ({ ...props }) => {
         className={clsx(style.input, modifier && style[`input--${modifier}`])}
         id={id}
         {...input_props}
-        value={meta.value}
       />
 
       {children}
