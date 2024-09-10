0eb8afe221e4546057ee50a919903a6afb18f2e3
Alexander <al.tulin@gmail.com>
2024-09-10 10:12:40 +0800

    add HidingHeader

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,23 +4,28 @@ import clsx from "clsx";
 import Links from "./Links";
 import Nav from "./Nav";
 import Logo from "@/UI/logo/Logo";
+import { HidingHeader } from "hiding-header-react";
+import "hiding-header/dist/style.css";
 
 const Header = () => {
   const [isMenu, setMenu] = useState(true);
 
-  // useEffect(() => {
-  //   yearRref.current!.innerHTML = new Date().getFullYear().toString();
-  // }, []);
   return (
-    <header className={clsx(style.header)}>
-      <div className={clsx(style.header__inner)}>
-        <Logo parent={"header"} />
+    <HidingHeader
+    // onHomeChange={(isHomeNew) => {
+    //   // console.log(isHomeNew);
+    // }}
+    >
+      <header className={clsx(style.header)}>
+        <div className={clsx(style.header__inner)}>
+          <Logo parent={"header"} />
 
-        <Nav isMenu={isMenu} setMenu={setMenu}>
-          <Links setMenu={setMenu} />
-        </Nav>
-      </div>
-    </header>
+          <Nav isMenu={isMenu} setMenu={setMenu}>
+            <Links setMenu={setMenu} />
+          </Nav>
+        </div>
+      </header>
+    </HidingHeader>
   );
 };
 export default Header;
